76d6255ee7865ddba8a23333703811f53ecf01a2
Sonu George / TheDarkMarshall <82310054+thedarkmarshall@users.noreply.github.com>
2024-02-27 19:41:04 +0530

    Update bres_circle.c
    Updated code. Fixed issues

--- bres_circle.c
+++ bres_circle.c
@@ -1,51 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-char arr[100][100] = {' '};
 
-void drawCircle(int xc, int yc, int x, int y);
-void circleBres(int xc, int yc, int r);
-void main() 
-{ 
-    // Example usage
-    circleBres(50, 50, 20);
+int arr[40][40] = {0};
 
-    // Printing the array
-    for (int i = 0; i < 100; i++) {
-        for (int j = 0; j < 100; j++) {
-            printf("%c ", arr[i][j]);
-        }
-        printf("\n");
-    }
-}
 void drawCircle(int xc, int yc, int x, int y) 
 { 
-    arr[xc+x][yc+y]='.';
-    arr[xc-x][yc+y]='.';
-    arr[xc+x][yc-y]='.';
-    arr[xc-x][yc-y]='.';
-    arr[xc+y][yc+x]='.';
-    arr[xc-y][yc+x]='.';
-    arr[xc+y][yc-x]='.';
-    arr[xc-y][yc-x]='.';
+    arr[xc + x][yc + y] = 1;
+    arr[xc - x][yc + y] = 1;
+    arr[xc + x][yc - y] = 1;
+    arr[xc - x][yc - y] = 1;
+    arr[xc + y][yc + x] = 1;
+    arr[xc - y][yc + x] = 1;
+    arr[xc + y][yc - x] = 1;
+    arr[xc - y][yc - x] = 1;
 } 
-  
 
-void circleBres(int xc, int yc, int r) 
+void circle(int xc, int yc, int r) 
 { 
     int x = 0, y = r; 
     int p = 3 - 2 * r; 
     drawCircle(xc, yc, x, y); 
     while (y >= x) 
     {
-        x++; 
+        x++;
         if (p > 0) 
         { 
             y--;  
-            p+=4*(x - y) + 10; 
+            p += 4 * (x - y) + 10; 
         } 
         else
-            p+=4*x + 6; 
+            p += 4 * x + 6; 
         drawCircle(xc, yc, x, y); 
     } 
 } 
 
+int main() 
+{ 
+    circle(20, 20, 10);
+    for (int i = 0; i < 40; i++) {
+        for (int j = 0; j < 40; j++) 
+            printf("%c", arr[i][j] ? '.' : ' ');
+        printf("\n");
+    }
+    return 0;
+}
